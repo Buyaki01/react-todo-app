10387d6361c42c6f1725b64d7bbf875494b6b0f2
Buyaki <rittahbuyaki@gmail.com>
2022-01-10 21:45:06 +0300

    Use the snapshot (prevState) within the setState() to get the previous state instead of using this.state

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -24,14 +24,14 @@ class TodoContainer extends React.Component {
   };
   
   handleChange = id => {
-    this.setState({    
-      todos: this.state.todos.map(todo => {      
-        if (todo.id === id) {       
-          todo.completed = !todo.completed;      
-        }      
-        return todo;    
-      }) 
-    });
+    this.setState(prevState => ({  
+      todos: prevState.todos.map(todo => {   
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      }),
+    }))
   };
 
   render() {
