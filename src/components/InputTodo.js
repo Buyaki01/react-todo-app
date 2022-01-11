96c8a3025d750bb333a0e0962ffea63f894568b2
Buyaki <rittahbuyaki@gmail.com>
2022-01-11 16:51:38 +0300

    Add a new item to the current todos list with hardcoded ID

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -3,19 +3,20 @@ import React, { Component } from "react"
 class InputTodo extends Component {
   state = {
     title: "",
-    email: "",
   };
   
   onChange = e => {
     this.setState({    
-      [e.target.name]: e.target.value 
+      title: e.target.value 
     });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state.title);
-    console.log(this.state.email);
+    this.props.addTodoProps(this.state.title);
+    this.setState({   
+      title: "",  
+    });
   };
 
   render() {
@@ -27,13 +28,6 @@ class InputTodo extends Component {
           name="title" 
           value={this.state.title} 
           onChange={this.onChange} 
-        />
-
-        <input 
-          name='email'
-          placeholder='Email' 
-          value = {this.state.email}
-          onChange={this.onChange}
         />
         <button>Submit</button>
       </form>
