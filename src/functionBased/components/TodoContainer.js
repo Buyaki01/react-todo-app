eb082f2531eeab60aae807292b8ed0544e636e67
Buyaki <rittahbuyaki@gmail.com>
2022-01-13 13:12:43 +0300

    Use useEffect in place of componentDidUpdate and componentDidMount

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo'; 
@@ -49,24 +49,22 @@ const TodoContainer = () => {
     )
   };
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if(prevState.todos !== this.state.todos) {
-  //     const temp = JSON.stringify(this.state.todos)
-  //     localStorage.setItem("todos", temp)
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   const temp = localStorage.getItem("todos")
-  //   const loadedTodos = JSON.parse(temp)
-  //   if (loadedTodos) {
-  //     this.setState({
-  //       todos: loadedTodos
-  //     })
-  //   }
-  // };
-
+  useEffect(() => {
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
   
+    if (loadedTodos) {
+      setTodos(loadedTodos)
+    }
+  }, [setTodos])
+
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
+
   return (
     <div className="container">
       <div className="inner">
