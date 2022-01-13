c742136baa92894d1ed09d20c2656eefa5e4732d
Buyaki <rittahbuyaki@gmail.com>
2022-01-13 10:53:41 +0300

    Use multiple input fields with useState

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -2,8 +2,7 @@ import React, { useState } from "react"
 
 const InputTodo = props => {
   const [inputText, setInputText] = useState({
-    fName: "",
-    lastName: "",
+    title: "",
   })
 
   const onChange = e => {
@@ -15,34 +14,28 @@ const InputTodo = props => {
 
   const handleSubmit = e => {
     e.preventDefault()
-    console.log("submitted")
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add first name"
-          value={inputText.fName}
-          name="fName"
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add last name"
-          value={inputText.lastName}
-          name="lastName"
-          onChange={onChange}
-        />
-
-        <button className="input-submit">Submit</button>
-      </form>
-      <h2>{inputText.fName}</h2>
-      <h2>{inputText.lastName}</h2>
-    </>
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
   )
 }
 
